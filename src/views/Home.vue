52d449cb8b9018a8996952ee4cbc647e9095d272
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-09 14:11:07 -0400

    Uso del calendario material design en conjunto con vue y axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!--
+      wrap es usado como true,
+      esto para que se respeten las 12 columnas
+    -->
+     <v-layout :wrap="true">
+      <!--
+        xs12: al configurar el punto de
+        quiebre mas pequeñor, automaticamente
+        este se hereda a los mas grandes, osea
+        se usaran 12 columnas para xs, sm, md, lg y xl
+      -->
+      <v-flex xs12>
+        <v-card>
+          <v-date-picker
+          v-model="fecha"
+          full-width
+          locale="es-cl"
+          :min="minimo"
+          :max="maximo"
+          @change="getDolar(fecha)"
+          >
+          </v-date-picker>
+        </v-card>
+        <v-card color="error" dark>
+          <v-card-text class="display-1 text-xs-center">
+            <!--  Valor del dolar -->
+            {{valor}}
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      fecha: new Date().toISOString().substring(0, 10), // Fecha seleccionada por el usuario
+      minimo: '1984', // valor minimo mostrado en el calendario
+      maximo: new Date().toISOString().substring(0, 10), // valor maximo mostrado en el calendario (el dia actual)
+      valor: null // Almacena el valor del dolar para mostrarlo bajo el calendario
+    }
+  },
+  methods: {
+    // Evento Asincrono que obtiene el valor del dolas desde un API
+    async getDolar (dia) {
+      let arrayFecha = dia.split('-')
+      let ddmmyy = arrayFecha[2] + '-' + arrayFecha[1] + '-' + arrayFecha[0]
+      try {
+        let datos = await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+        // Valido que contenga datos, en caso de dia sabado, domingo o festivo que no entrega el valor del dolas
+        if (datos.data.serie.length > 0) {
+          this.valor = await datos.data.serie[0].valor
+        } else {
+          this.valor = 'Sin resultados'
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  created () {
+    this.getDolar(this.fecha)
   }
 }
 </script>
