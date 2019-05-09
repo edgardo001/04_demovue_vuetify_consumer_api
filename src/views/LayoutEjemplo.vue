34f10300fc499dcb149054d0f5e7525d4637fc24
Edgardo vasquez <edgardo.a.v@gmail.com>
2019-05-09 14:10:10 -0400

    Ejemplos del sistema de layout de Vuetify

---
+++ src/views/LayoutEjemplo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--
+      wrap es usado como true,
+      esto para que se respeten las 12 columnas
+    -->
+     <v-layout :wrap="true">
+      <v-flex xs6 md6>
+        <h1>Hola mundo!</h1>
+      </v-flex>
+      <v-flex xs2 md3>
+        <h1>Hola mundo!</h1>
+      </v-flex>
+      <v-flex xs4 md3>
+        <h1>Hola mundo!</h1>
+      </v-flex>
+      <v-flex xs4 md3>
+        <h1>Hola mundo!</h1>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
